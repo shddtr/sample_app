82fabb20558de419146d59e61e0353a37bcedde8
shddtr <shddtr@gmail.com>
2017-06-19 17:12:18 +0900

    Finish user signup

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,4 +1,32 @@
+# coding: utf-8
 class UsersController < ApplicationController
+  def show
+    @user = User.find(params[:id])
+    # debugger
+  end
+
   def new
+    @user = User.new
+    # debugger
+  end
+
+  def create
+    # debugger
+    @user = User.new(user_params)
+    if @user.save
+      # :successは慣習的
+      flash[:success] = "Welcome to the Sample App!"
+      # 以下のコードは等価
+      # redirect_to user_url(@user)
+      redirect_to @user
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:name, :email, :password,
+                                 :password_confirmation)
   end
 end
