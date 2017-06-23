e4f7e8d6fd80ef9bffaca208f2c6e7fbc25561f4
shddtr <shddtr@gmail.com>
2017-06-23 10:07:40 +0900

    Add account activation

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,11 +1,12 @@
 # coding: utf-8
 class User < ApplicationRecord
   has_many :microposts, dependent: :destroy
-  attr_accessor :remember_token
+  attr_accessor :remember_token, :activation_token
 
   # Userモデルの中では右式のselfを省略できる
   # before_save {self.email = email.downcase}
-  before_save {self.email.downcase!}
+  before_save :downcase_email
+  before_create :create_activation_digest
   validates :name, presence: true, length: {maximum: 50}
   VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   validates :email, presence: true, length: {maximum: 255},
@@ -31,9 +32,10 @@ class User < ApplicationRecord
     update_attribute(:remember_digest, User.digest(remember_token))
   end
 
-  def authenticated?(remember_token)
-    return false if remember_digest.nil?
-    BCrypt::Password.new(remember_digest).is_password?(remember_token)
+  def authenticated?(attribute, token)
+    digest = send("#{attribute}_digest")
+    return false if digest.nil?
+    BCrypt::Password.new(digest).is_password?(token)
   end
 
   def forget
@@ -42,5 +44,25 @@ class User < ApplicationRecord
 
   def feed
     Micropost.where("user_id = ?", id)
+  end
+
+  def activate
+    # update_attribute(:activated, true)
+    # update_attribute(:activated_at, Time.zone.now)
+    update_columns(activated: true, activated_at: Time.zone.now)
+  end
+
+  def send_activation_email
+    UserMailer.account_activation(self).deliver_now
+  end
+
+  private
+  def downcase_email
+    self.email.downcase!
+  end
+
+  def create_activation_digest
+    self.activation_token = User.new_token
+    self.activation_digest = User.digest(activation_token)
   end
 end
--- test/integration/users_signup_test.rb
+++ test/integration/users_signup_test.rb
@@ -2,6 +2,11 @@
 require 'test_helper'
 
 class UsersSignupTest < ActionDispatch::IntegrationTest
+  def setup
+    # メッセージ数をクリア
+    ActionMailer::Base.deliveries.clear
+  end
+
   test "invalid signup information" do
     get signup_path
     assert_no_difference 'User.count' do
@@ -19,7 +24,7 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
     assert_select 'form[action="/signup"]'
   end
 
-  test "valid signup information" do
+  test "valid signup information with account activation" do
     get signup_path
     assert_difference 'User.count', 1 do
       post signup_path, params: {user: {name: "Example User",
@@ -27,10 +32,27 @@ class UsersSignupTest < ActionDispatch::IntegrationTest
                                        password: "password",
                                        password_confirmation: "password"}}
     end
-    # POSTリクエストを送信した結果を見て指定されたリダイレクト先に移動するメソッド
+    # メッセージが送信されていることの確認
+    assert_equal 1, ActionMailer::Base.deliveries.size
+    # assignsはこのファイルに対応するコントローラではなく、
+    # この辞典で対応するコントローラのインスタンス変数へアクセスできる。
+    # (この場合はpost signup_pathなのでUsersController#create内の@user)
+    user = assigns(:user)
+    assert_not user.activated?
+    # 有効化していない状態でログイン
+    log_in_as(user)
+    assert_not is_logged_in?
+    # 不正なトークン
+    get edit_account_activation_path("invalid token", email: user.email)
+    assert_not is_logged_in?
+    # 不正なアドレス
+    get edit_account_activation_path(user.activation_token, email: 'wrong')
+    assert_not is_logged_in?
+    # 正しいトークンとアドレス
+    get edit_account_activation_path(user.activation_token, email: user.email)
+    assert user.reload.activated?
     follow_redirect!
     assert_template 'users/show'
     assert is_logged_in?
-    assert_not flash.empty?
   end
 end
